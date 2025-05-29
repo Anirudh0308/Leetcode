1b48b8667d916d5d04a1db7e716cf098237c020f
Anirudh0308 <129490209+Anirudh0308@users.noreply.github.com>
2025-05-29 23:39:35 +0530

    Time: 2 ms (46.16%), Space: 13.3 MB (20.95%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+// Fraz
+
+    void help(int i, vector<int>&nums,vector<int>&temp, vector<vector<int>>&ans,bool pre){
+        if(i==nums.size()){
+            ans.push_back(temp);
+        }
+        else{
+            help(i+1,nums,temp,ans,false);
+            if(i>0&&nums[i]==nums[i-1]&&(!pre))
+                return;
+            
+            temp.push_back(nums[i]);
+            help(i+1,nums,temp,ans,true);
+
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int>temp;
+        help(0,nums,temp,ans,false);
+        return ans;
+    }
+};
