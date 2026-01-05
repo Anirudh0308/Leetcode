9432f226b9dd01debbfef9b6e2fd3e38b407ad83
Anirudh Kumar Yadav <129490209+Anirudh0308@users.noreply.github.com>
2026-01-05 11:32:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,15 +1,32 @@
+// Approach - 1 (using set)
+
+// class Solution {
+// public:
+//     int repeatedNTimes(vector<int>& nums) {
+//         unordered_set<int> st;
+        
+//         for (int &num : nums) {
+//             if(st.count(num))
+//                 return num;
+            
+//             st.insert(num);
+//         }
+        
+//         return -1;
+//     }
+// };
+
+
+// Approach -2
+
+
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_set<int> st;
-        
-        for (int &num : nums) {
-            if(st.count(num))
-                return num;
-            
-            st.insert(num);
+        for(int i=2; i< nums.size(); i++){
+            if(nums[i] == nums[i-1] || nums[i-2])
+                return nums[i];
         }
-        
-        return -1;
+        return nums.back();
     }
 };
